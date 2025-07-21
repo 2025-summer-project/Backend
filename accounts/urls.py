7ae08a48f13e81e7955a6ac8c3f0550226326477
jaemin03 <jeongjaemin60@gmail.com>
2025-07-21 21:27:10 +0900

    로그인 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import SignupView
+from .views import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 # accounts 앱의 URL 경로 설정
 urlpatterns = [
-    path('signup', SignupView.as_view(), name='signup'),  # POST /auth/signup 으로 연결됨
+    path('signup', SignupView.as_view(), name='signup'),                            # POST /auth/signup
+    path('login', CustomTokenObtainPairView.as_view(), name='login'),               # POST /auth/login
+    path('refresh', TokenRefreshView.as_view(), name='token_refresh'),              # POST /auth/refresh
 ]
