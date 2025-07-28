565bacb3dbaecd75b08b43cd9a203db874b2e4c7
jaemin03 <jeongjaemin60@gmail.com>
2025-07-28 20:55:50 +0900

    로그아웃 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import SignupView
-from .views import CustomTokenObtainPairView
+from .views import SignupView, LoginView, LogoutView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 # accounts 앱의 URL 경로 설정
 urlpatterns = [
     path('signup', SignupView.as_view(), name='signup'),                            # POST /auth/signup
-    path('login', CustomTokenObtainPairView.as_view(), name='login'),               # POST /auth/login
+    path('login', LoginView.as_view(), name='login'),               # POST /auth/login
+    path('logout', LogoutView.as_view(), name='logout'),              # POST /auth/refresh
     path('refresh', TokenRefreshView.as_view(), name='token_refresh'),              # POST /auth/refresh
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import serializers
+from core.models import RefreshTokenStore
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import CustomTokenObtainPairSerializer
 from accounts.serializers import UserSerializer  # 시리얼라이저 불러오기
@@ -29,15 +30,8 @@ class SignupView(APIView):
             print("유효성 실패:", serializer.errors)  # 실패 시 오류 로그 확인
             return Response(serializer.errors, status=400)
         
-# JWT 로그인
-#token_obtain_schema = swagger_auto_schema(
-   # operation_summary="JWT 로그인",
-   # request_body=CustomTokenObtainPairSerializer,
-    #responses={200: openapi.Response("로그인 성공")},
-#)
-
-#@method_decorator(token_obtain_schema, name='post')
-class CustomTokenObtainPairView(TokenObtainPairView):
+# 로그인 요청 처리
+class LoginView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
     @swagger_auto_schema(
@@ -62,3 +56,34 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     )
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
+    
+# 로그아웃 요청 처리   
+class LogoutView(APIView):
+
+    @swagger_auto_schema(
+        operation_summary="로그아웃",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=["refresh"],
+            properties={
+                "refresh": openapi.Schema(type=openapi.TYPE_STRING, description="리프레시 토큰"),
+            }
+        ),
+        responses={
+            200: "로그아웃 성공",
+            400: "요청 에러",
+        }
+    )    
+
+    def post(self, request):
+        refresh_token = request.data.get("refresh")
+
+        if not refresh_token:
+            return Response({"detail": "refresh 토큰이 필요합니다."}, status=400)
+
+        deleted_count, _ = RefreshTokenStore.objects.filter(token=refresh_token).delete()
+        
+        if deleted_count:
+            return Response({"detail": "로그아웃 성공"}, status=200)
+        else:
+           return Response({"detail": "해당 토큰이 존재하지 않거나 이미 삭제됨"}, status=400)
